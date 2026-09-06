d827353c1ab71d1d31c5c17effa16dd81134075f
Yaman5 <its.yamanadeeb.0@gmail.com>
2026-09-07 00:54:25 +0300

    feat: Add Product model with JSON serialization and sample data for testing

--- Learning-Flutter/store_app/super_store/lib/main.dart
+++ Learning-Flutter/store_app/super_store/lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:super_store/models/product.dart';
 
 void main() {
+  // Hardcoded test for the model
+  final Map<String, dynamic> fakeApiData = {
+    "id": 1,
+    "title": "Essence Mascara Lash Princess",
+    "description": "The Essence Mascara Lash Princess is a popular mascara...",
+    "category": "beauty",
+    "price": 9.99,
+    "rating": 2.56,
+    "tags": ["beauty", "mascara"],
+    "brand": "Essence",
+    "availabilityStatus": "In Stock",
+    "returnPolicy": "No return policy",
+    "images": ["https://.../1.webp"],
+    "thumbnail": "https://.../thumbnail.webp",
+  };
+
+  final myTestProduct = Product.fromJson(fakeApiData);
+  print(myTestProduct);
+
   runApp(const MyApp());
 }
 
---
+++ Learning-Flutter/store_app/super_store/lib/models/product.dart
@@ -0,0 +1,67 @@
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final String category;
+  // Price and rating are not mutable by design.
+  // Howeever, it may be better to be mutable and add methods `discountPercentage` and calcolate at the cart logic.
+  // I will keep this in mind for later.
+  double price;
+  double rating;
+  final List<String> tags;
+  final String brand;
+  final String availabilityStatus;
+  final String returnPolicy;
+  final List<String> images;
+  final String thumbnail;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.price,
+    required this.rating,
+    required this.tags,
+    required this.brand,
+    required this.availabilityStatus,
+    required this.returnPolicy,
+    required this.images,
+    required this.thumbnail,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      category: json['category'],
+      price: json['price'].toDouble(),
+      rating: json['rating'].toDouble(),
+      tags: json['tags'],
+      brand: json['brand'],
+      availabilityStatus: json['availabilityStatus'],
+      returnPolicy: json['returnPolicy'],
+      images: json['images'],
+      thumbnail: json['thumbnail'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Product('
+        'id: $id, '
+        'title: $title, '
+        'description: $description, '
+        'category: $category, '
+        'price: $price, '
+        'rating: $rating, '
+        'tags: $tags, '
+        'brand: $brand, '
+        'availabilityStatus: $availabilityStatus, '
+        'returnPolicy: $returnPolicy, '
+        'images: $images, '
+        'thumbnail: $thumbnail'
+        ')';
+  }
+}
